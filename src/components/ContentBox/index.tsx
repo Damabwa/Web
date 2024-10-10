93308ff45823e1384c5bbcced760572b7b68397e
seorinn <seorin@ajou.ac.kr>
2024-10-11 05:50:10 +0900

    feat: PC에서 좌우 스크롤 대신 '>', '<' 버튼 추가

--- src/components/ContentBox/index.tsx
+++ src/components/ContentBox/index.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import icn_back from "../../assets/svgs/icn_back.svg";
+import icn_next from "../../assets/svgs/icn_next.svg";
 import icn_clip from "../../assets/svgs/icn_clip.svg";
 import icn_time from "../../assets/svgs/icn_time.svg";
 import icn_location from "../../assets/svgs/icn_location.svg";
@@ -19,12 +21,38 @@ interface Props {
 }
 
 export default function ContentBox({ data }: Props) {
+  const scrollRef = useRef<HTMLInputElement>(null);
   const navigation = useNavigate();
+
+  const [isPC, setIsPC] = useState(true);
+  const [isNext, setIsNext] = useState(true);
   const [isClipped, setIsClipped] = useState(false);
   const clipCount = data.clipCount > 99 ? "99+" : data.clipCount;
 
+  const handleResize = () => {
+    setIsPC(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleNext = () => {
+    if (isPC && scrollRef.current) {
+      scrollRef.current.scrollBy(
+        isNext
+          ? { left: 300, behavior: "smooth" }
+          : { left: -300, behavior: "smooth" }
+      );
+    }
+    setIsNext(!isNext);
+  };
+
   return (
-    <div className="flex flex-col gap-2 py-4 cursor-pointer">
+    <div className="relative flex flex-col gap-2 py-4 cursor-pointer">
       <div className="flex justify-between px-4 ">
         <div className="flex flex-col font-pre">
           <span className="text-xl font-bold">{data.title}</span>
@@ -46,13 +74,25 @@ export default function ContentBox({ data }: Props) {
           <span className="w-6 text-xs">{clipCount}</span>
         </div>
       </div>
-      <div className="flex w-full gap-3 px-4 overflow-x-auto">
+      <div className="flex w-full gap-3 px-4 overflow-x-auto" ref={scrollRef}>
         {data.images.map((image) => (
           <div className="gap-2">
             <div className="w-[7.5rem] h-[7.5rem] rounded-lg bg-gray" />
           </div>
         ))}
       </div>
+      {isPC && (
+        <div
+          className={`flex top-12 mt-6 absolute items-center h-32 w-12 from-[rgba(255,255,255,0)] to-[rgba(255,255,255,1)] ${isNext ? "bg-gradient-to-r justify-end right-0 " : "bg-gradient-to-l justify-start left-0"}`}
+        >
+          <img
+            className={`${isNext ? "pr-2" : "pl-2"} cursor-pointer`}
+            alt=""
+            src={isNext ? icn_next : icn_back}
+            onClick={() => handleNext()}
+          />
+        </div>
+      )}
       <div className="flex flex-col gap-1 pl-4 text-[0.625rem]">
         <div className="flex gap-[0.12rem]">
           <img alt="" src={icn_time} />
--- src/pages/MainHome/EventBox/index.tsx
+++ src/pages/MainHome/EventBox/index.tsx
@@ -1,5 +1,7 @@
+import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import icn_next from "../../../assets/svgs/icn_next.svg";
+import icn_back from "../../../assets/svgs/icn_back.svg";
 import icn_camera from "../../../assets/svgs/icn_camera.svg";
 import icn_calendar from "../../../assets/svgs/icn_calendar.svg";
 
@@ -34,16 +36,47 @@ export default function EventBox({ title, subTitle }: Props) {
       sub: "스냅 사진",
       period: "08.29 - 08.30",
     },
+    {
+      image: "",
+      name: "이벤트5",
+      sub: "스냅 사진",
+      period: "08.29 - 08.30",
+    },
   ];
 
+  const scrollRef = useRef<HTMLInputElement>(null);
   const navigation = useNavigate();
+  const [isPC, setIsPC] = useState(true);
+  const [isNext, setIsNext] = useState(true);
+
+  const handleResize = () => {
+    setIsPC(window.innerWidth > 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleNext = () => {
+    if (isPC && scrollRef.current) {
+      scrollRef.current.scrollBy(
+        isNext
+          ? { left: 300, behavior: "smooth" }
+          : { left: -300, behavior: "smooth" }
+      );
+    }
+    setIsNext(!isNext);
+  };
 
   const navigateEvent = () => {
     navigation("/events");
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="relative flex flex-col">
       <div className="flex justify-between px-4 mb-3">
         <div className="flex flex-col font-pre" onClick={navigateEvent}>
           <span className="text-xl font-bold cursor-pointer">{title}</span>
@@ -59,7 +92,10 @@ export default function EventBox({ title, subTitle }: Props) {
           <img className="w-3 h-3" alt=">" src={icn_next} />
         </div>
       </div>
-      <div className="flex w-full gap-3 px-4 pb-4 overflow-x-auto">
+      <div
+        className="flex w-full gap-3 px-4 pb-4 overflow-x-auto select-none"
+        ref={scrollRef}
+      >
         {mockdata.map((item) => (
           <div key={item.name} className="flex flex-col gap-2 cursor-pointer">
             {/* <img alt="image" src={}/> */}
@@ -80,6 +116,18 @@ export default function EventBox({ title, subTitle }: Props) {
           </div>
         ))}
       </div>
+      {isPC && (
+        <div
+          className={`flex top-12 absolute items-center h-32 w-12 from-[rgba(255,255,255,0)] to-[rgba(255,255,255,1)] ${isNext ? "bg-gradient-to-r justify-end right-0 " : "bg-gradient-to-l justify-start left-0"}`}
+        >
+          <img
+            className={`${isNext ? "pr-2" : "pl-2"} cursor-pointer`}
+            alt=""
+            src={isNext ? icn_next : icn_back}
+            onClick={() => handleNext()}
+          />
+        </div>
+      )}
     </div>
   );
 }
