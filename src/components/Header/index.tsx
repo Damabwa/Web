8f49e3289d06a180c6f758d4b512e9b34382511e
seorinn <seorin@ajou.ac.kr>
2024-10-11 05:49:40 +0900

    style: 헤더 z index 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Header({ title, left, right }: Props) {
   return (
-    <div className="fixed top-0 flex items-center justify-between h-12 px-4 py-1 w-96 bg-violet600">
+    <div className="fixed top-0 z-20 flex items-center justify-between h-12 px-4 py-1 w-96 bg-violet600">
       <div>{left}</div>
       <div className="text-2xl font-bold text-center text-white cursor-pointer font-pre">
         {title}
