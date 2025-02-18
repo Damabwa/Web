eda19abe620e00ef2142d9d02d53724839443707
seorinn <seorin@ajou.ac.kr>
2025-02-19 00:13:06 +0900

    fix: 작가 페이지 간격 수정

--- src/components/MorePhotographerInfo/index.tsx
+++ src/components/MorePhotographerInfo/index.tsx
@@ -27,12 +27,15 @@ export default function MorePhotographerInfo({
       </div>
     );
   return (
-    <div className="flex flex-col min-w-full px-4 py-6 font-bold bg-white gap-7">
-      <div className="flex flex-col w-full gap-2">
+    <div className="flex flex-col min-w-full py-6 font-bold bg-white gap-7">
+      <div className="flex flex-col w-full gap-2 pl-4">
         <div>포트폴리오</div>
         <div className="flex w-full gap-2 overflow-x-scroll">
           {portfolio.map((image, index) => (
-            <div key={index}>
+            <div
+              key={index}
+              className={`${index === portfolio.length - 1 && "mr-4"}`}
+            >
               <div className="w-[7.5rem] h-[7.5rem]">
                 <img
                   className="object-cover w-full h-full cursor-pointer"
@@ -44,11 +47,11 @@ export default function MorePhotographerInfo({
           ))}
         </div>
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 px-4">
         <div>상세 주소</div>
         <div className="text-sm font-medium text-black02">{address}</div>
       </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 px-4">
         <div>작가님 인사말</div>
         <div className="text-sm font-medium text-black02">{description}</div>
       </div>
