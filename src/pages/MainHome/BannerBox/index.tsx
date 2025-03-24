3bff189d7bfcea4e844d030e2c44b1f0b5ba44fa
seorinn <seorin@ajou.ac.kr>
2025-03-25 00:59:05 +0900

    feat: swiper slide 적용

--- src/pages/MainHome/BannerBox/index.tsx
+++ src/pages/MainHome/BannerBox/index.tsx
@@ -1,111 +1,37 @@
-import { useEffect, useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
 import banner_main from "../../../assets/imgs/banner_damaba_main.png";
 import banner_event from "../../../assets/imgs/banner_event1.png";
+import { useState } from "react";
 
 export default function BannerBox() {
-  const realBanners = [banner_main, banner_event];
-  const banners = [
-    realBanners[realBanners.length - 1],
-    ...realBanners,
-    realBanners[0],
-  ];
-  const [currentIndex, setCurrentIndex] = useState(1);
-  const containerRef = useRef<HTMLDivElement>(null);
-  const timerRef = useRef<NodeJS.Timeout | null>(null);
-
-  const scrollToIndex = (
-    index: number,
-    behavior: ScrollBehavior = "smooth"
-  ) => {
-    const container = containerRef.current;
-    if (!container) return;
-    const width = container.clientWidth;
-    container.scrollTo({ left: width * index, behavior });
-  };
-
-  const resetAutoSlide = () => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current);
-    }
-    timerRef.current = setTimeout(() => {
-      setCurrentIndex((prev) => prev + 1);
-    }, 4000);
-  };
-
-  useEffect(() => {
-    resetAutoSlide();
-    return () => {
-      if (timerRef.current) clearTimeout(timerRef.current);
-    };
-  }, [currentIndex]);
-
-  useEffect(() => {
-    const container = containerRef.current;
-    if (!container) return;
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            const index = Number(entry.target.getAttribute("data-index"));
-            setCurrentIndex(index);
-          }
-        });
-      },
-      {
-        root: container,
-        threshold: 0.5,
-      }
-    );
-
-    const slides = container.querySelectorAll(".slide-item");
-    slides.forEach((slide) => observer.observe(slide));
-
-    return () => {
-      slides.forEach((slide) => observer.unobserve(slide));
-    };
-  }, []);
-
-  useEffect(() => {
-    scrollToIndex(currentIndex);
-    const container = containerRef.current;
-    if (!container) return;
-
-    if (currentIndex === 0) {
-      setTimeout(() => {
-        scrollToIndex(realBanners.length, "smooth");
-        setCurrentIndex(realBanners.length);
-      }, 500);
-    } else if (currentIndex === banners.length - 1) {
-      setTimeout(() => {
-        scrollToIndex(1, "auto");
-        setCurrentIndex(1);
-      }, 500);
-    }
-  }, [currentIndex]);
+  const images = [banner_main, banner_event];
+  const [idx, setIdx] = useState(1);
 
   return (
-    <div className="relative shadow-banner rounded-xl">
-      <div
-        className="overflow-x-auto overflow-y-hidden snap-x snap-mandatory scroll-smooth"
-        ref={containerRef}
-        onScroll={() => resetAutoSlide()}
+    <div className="relative w-full overflow-hidden bg-gray100 rounded-xl shadow-banner">
+      <Swiper
+        centeredSlides={true}
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        modules={[Autoplay]}
+        onSlideChange={() => {
+          setIdx(idx > images.length - 1 ? 1 : idx + 1);
+        }}
       >
-        <div className="flex w-full">
-          {banners.map((image: any, index: number) => (
-            <div
-              key={index}
-              className="flex-shrink-0 w-full slide-item snap-center"
-              data-index={index}
-            >
-              <img src={image} className="object-cover min-w-full rounded-xl" />
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="absolute z-10 flex items-center h-4 px-3 py-2 text-xs text-white bg-black left-3 bottom-2 bg-opacity-30 rounded-2xl">
-        {((currentIndex - 1 + realBanners.length) % realBanners.length) + 1}/
-        {realBanners.length}
+        {images.map((img: string, index: number) => (
+          <SwiperSlide key={index}>
+            <img className="object-cover w-full h-full" src={img} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className="absolute z-10 w-10 py-[1px] text-xs font-medium text-center text-white bg-black bottom-2 left-3 bg-opacity-35 rounded-2xl">
+        {idx}/{images.length}
       </div>
     </div>
   );
