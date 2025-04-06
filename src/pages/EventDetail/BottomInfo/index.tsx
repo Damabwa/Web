decb37c7c6229253e923666f80fc9f27db8407c5
seorinn <seorin@ajou.ac.kr>
2025-04-06 19:41:19 +0900

    fix: 탈퇴회원의 이벤트 상세 내 작가 정보 미표기

--- src/pages/EventDetail/BottomInfo/index.tsx
+++ src/pages/EventDetail/BottomInfo/index.tsx
@@ -8,28 +8,29 @@ export default function BottomInfo({ promotionData }: Props) {
   const navigation = useNavigate();
   return (
     <div className="flex flex-col px-4 py-6 bg-white">
-      {promotionData.author.roles.includes("PHOTOGRAPHER") && (
-        <div>
-          <div className="pb-3 font-bold">촬영 작가</div>
-          <div className="flex items-center gap-2 pb-6">
-            <img
-              className="object-cover w-10 h-10 rounded-full cursor-pointer"
-              src={promotionData.author.profileImage.url}
-              onClick={() =>
-                navigation(`/photographer/${promotionData.author.id}`)
-              }
-            />
-            <div
-              className="cursor-pointer"
-              onClick={() =>
-                navigation(`/photographer/${promotionData.author.id}`)
-              }
-            >
-              {promotionData.author.nickname}
+      {promotionData.author &&
+        promotionData.author.roles.includes("PHOTOGRAPHER") && (
+          <div>
+            <div className="pb-3 font-bold">촬영 작가</div>
+            <div className="flex items-center gap-2 pb-6">
+              <img
+                className="object-cover w-10 h-10 rounded-full cursor-pointer"
+                src={promotionData.author.profileImage.url}
+                onClick={() =>
+                  navigation(`/photographer/${promotionData.author.id}`)
+                }
+              />
+              <div
+                className="cursor-pointer"
+                onClick={() =>
+                  navigation(`/photographer/${promotionData.author.id}`)
+                }
+              >
+                {promotionData.author.nickname}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
       <div>
         <div className="pb-3 font-bold">상세 설명</div>
         <div className="pb-5 text-sm font-medium text-black02">
