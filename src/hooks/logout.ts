67f4324a128ed65bb3dd25445f8849cac3055f5c
seorinn <seorin@ajou.ac.kr>
2025-04-07 00:13:13 +0900

    fix: hook 이중사용 x

--- src/hooks/logout.ts
+++ src/hooks/logout.ts
@@ -1,10 +1,8 @@
 import { setRecoil } from "recoil-nexus";
-import { useNavigate } from "react-router-dom";
 import { userState } from "../atom/atom";
 
 export const logout = () => {
-  const navigation = useNavigate();
   setRecoil(userState, { id: -1, roles: [] });
   localStorage.removeItem("accessToken");
-  navigation(`/`);
+  window.location.href = "/";
 };
