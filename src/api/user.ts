393956c42f3f5e21339c06b37b32fcf72914bcd6
seorinn <seorin@ajou.ac.kr>
2025-05-28 20:29:47 +0900

    feat: 회원 탈퇴 기능 적용

--- src/api/user.ts
+++ src/api/user.ts
@@ -5,10 +5,12 @@ export const refresh = async () => await GET("/refresh-token", true);
 export const checkUserExistence = async (nickname: string) =>
   await GET(`/users/nicknames/existence?nickname=${nickname}`);
 
+export const getUserInfo = async () => await GET(`/users/me`, true);
+
+export const deleteUser = async () => await DELETE(`/users/me`, true);
+
 export const userRegistration = async (body: any) =>
   await POST(`/users/me/registration`, body, true);
-
-export const getUserInfo = async () => await GET(`/users/me`, true);
 
 export const modifyProfile = async (body: any) =>
   await PUT(`/users/me/profile`, body, true);
