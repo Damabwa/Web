2abf6a1311110f72dec3a845cab7c64177a46f8d
seorinn <seorin@ajou.ac.kr>
2026-05-08 15:23:43 +0900

    Revert "refactor: [#18] onClick* 내부 핸들러 handle* 컨벤션으로 수정 및 누락 Func 접미사 제거"
    This reverts commit 2585abe5aece016aa69c7e938e1da139407a03e9.

--- src/components/PromotionBox/index.tsx
+++ src/components/PromotionBox/index.tsx
@@ -37,11 +37,11 @@ export default function PromotionBox({ data }: Props) {
     setSaveCount(data.saveCount);
   }, [data]);
 
-  const handleSave = () => {
-    requireLogin(() => toggleSavePromotion(isClipped));
+  const onClickSave = () => {
+    requireLogin(() => savePromotionFunc(isClipped));
   };
 
-  const toggleSavePromotion = async (isClipped: boolean) => {
+  const savePromotionFunc = async (isClipped: boolean) => {
     try {
       setIsClipped(!isClipped);
       setSaveCount(isClipped ? saveCount - 1 : saveCount + 1);
@@ -86,7 +86,7 @@ export default function PromotionBox({ data }: Props) {
           <img
             alt="clip"
             src={isClipped ? icn_clipOn : icn_clipOff}
-            onClick={() => handleSave()}
+            onClick={() => onClickSave()}
           />
         </div>
       </div>
--- src/pages/MainHome/index.tsx
+++ src/pages/MainHome/index.tsx
@@ -17,7 +17,7 @@ function MainHome() {
   const { showLoginModal, loginModalProps } = useLoginGuard();
 
   useEffect(() => {
-    const checkAndShowLoginPopup = () => {
+    const showLoginPopupFunc = () => {
       if (
         !localStorage.getItem("accessToken") &&
         !sessionStorage.getItem("hasVisited")
@@ -32,22 +32,22 @@ function MainHome() {
       }
     };
 
-    const fetchUserInfo = async () => {
-      if (checkAndShowLoginPopup()) return;
+    const getUserInfoFunc = async () => {
+      if (showLoginPopupFunc()) return;
       else if (localStorage.getItem("accessToken")) {
         try {
           await getUserInfo();
         } catch (e: any) {
         } finally {
-          checkAndShowLoginPopup();
+          showLoginPopupFunc();
         }
       }
     };
 
-    fetchUserInfo();
+    getUserInfoFunc();
   }, []);
 
-  const handleMyPageClick = () => {
+  const onClickMyPage = () => {
     if (!localStorage.getItem("accessToken")) loginModalProps.setShowModal(true);
     else navigation(`/mypage`);
   };
@@ -72,7 +72,7 @@ function MainHome() {
                 className="mr-4"
                 src={icn_mypage}
                 alt="마이페이지"
-                onClick={() => handleMyPageClick()}
+                onClick={() => onClickMyPage()}
               />
             </div>
           }
