a42c280502056968e42d69a4a41dbdaf2c479e33
seorinn <seorin@ajou.ac.kr>
2026-05-11 15:41:19 +0900

    fix: [TA-209] ButtonActive 비활성 상태에서도 onClick 호출됨

--- src/components/ButtonActive/index.tsx
+++ src/components/ButtonActive/index.tsx
@@ -7,8 +7,8 @@ interface Props {
 export default function ButtonActive({ activation, onClick, text }: Props) {
   return (
     <button
-      className={`w-full h-[3.25rem] mb-4 text-white outline-none rounded-xl font-semibold ${activation ? "bg-violet400" : "bg-darkgray cursor-default"}`}
-      onClick={() => onClick()}
+      className={`w-full h-[3.25rem] mb-4 text-white outline-none rounded-xl font-semibold ${activation ? "bg-violet400" : "bg-darkgray cursor-not-allowed"}`}
+      onClick={onClick}
       disabled={!activation}
     >
       {text}
