55f69a9c99aa75ed8088dd5c8a8c6cc1b57eb1f9
seorinn <seorin@ajou.ac.kr>
2026-05-11 15:51:50 +0900

    fix: [TA-183] API 함수명 수정 (savePhotographer, savePromotion) 및 lazy loading/onError 재적용

--- src/components/PromotionBox/index.tsx
+++ src/components/PromotionBox/index.tsx
@@ -9,6 +9,7 @@ import icn_clipOff from "../../assets/svgs/icn_clip.svg";
 import icn_clipOn from "../../assets/svgs/icn_clipOn.svg";
 import icn_time from "../../assets/svgs/icn_event_home_clock.svg";
 import icn_location from "../../assets/svgs/icn_event_home_location.svg";
+import icn_camera from "../../assets/svgs/icn_camera.svg";
 import ModalCheck from "../ModalCheck";
 
 interface postData {
@@ -108,6 +109,11 @@ export default function PromotionBox({ data }: Props) {
                 className="object-cover min-w-full min-h-full"
                 src={image.url}
                 alt="이벤트 이미지"
+                loading="lazy"
+                onError={(e) => {
+                  e.currentTarget.onerror = null;
+                  e.currentTarget.src = icn_camera;
+                }}
               />
             </div>
           </div>
